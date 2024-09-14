acad9eb3ef29b61213f3fe7b269e694ffb4dcc2a
Peace Mastre <116195365+peacePaull@users.noreply.github.com>
2024-09-15 00:57:09 +0100

    implemented CQRS

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,6 @@
+﻿namespace Catalog.API.Products.CreateProduct
+{
+    public class CreateProductEndpoint //dis will represent our presentation API Layer
+    {
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : IRequest<CreateProductResult>; //CreateProductCommand represents the data that we need to create a new product.
+public record CreateProductResult(Guid Id); //CreateProductResult represents the response object return object,which in this case we just returned the product id.
+internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult> //dis class contains the business logic to handle create product command.
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        //Business logic to create a product
+        throw new NotImplementedException();
+    }
+}
+
+
